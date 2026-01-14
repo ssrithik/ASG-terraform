ea30f7804e0cc57138e70ccfdc49b81b2449de1c
ssrithik <166838233+ssrithik@users.noreply.github.com>
2026-01-14 09:28:36 +0530

    Scaling

---
+++ scaling.tf
@@ -0,0 +1,26 @@
+resource "aws_autoscaling_policy" "scale_out" {
+  name                   = "cpu-scale-out"
+  autoscaling_group_name = aws_autoscaling_group.app_asg.name
+  adjustment_type        = "ChangeInCapacity"
+  scaling_adjustment     = 1
+  cooldown               = 300
+}
+
+resource "aws_cloudwatch_metric_alarm" "cpu_high" {
+  alarm_name          = "cpu-utilization-high"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 2
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = 60
+  statistic           = "Average"
+  threshold           = 20
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.app_asg.name
+  }
+
+  alarm_actions = [
+    aws_autoscaling_policy.scale_out.arn
+  ]
+}
